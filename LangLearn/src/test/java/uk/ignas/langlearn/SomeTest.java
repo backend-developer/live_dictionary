ab0320250fcc4ac71bc51af86df67b4842da7f1e
TIgnas <java.backend.developer@outlook.com>
2015-12-16 14:45:11 +0000

    #22 [Ignas]  finally we have first out simple unit test

---
+++ LangLearn/src/test/java/uk/ignas/langlearn/SomeTest.java
@@ -0,0 +1,16 @@
+package uk.ignas.langlearn;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ignas on 12/16/15.
+ */
+public class SomeTest {
+    @Test
+    public void someTest() {
+        Assert.assertTrue(true);
+    }
+}
