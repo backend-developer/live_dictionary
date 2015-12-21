ecd4cd49d49354d9a728a1081f6c970cfd8f32ea
TIgnas <java.backend.developer@outlook.com>
2015-12-21 11:08:07 +0000

    #36 [Ignas] data importing related methods moved to DataImporterExporter

--- LangLearn/src/main/java/uk/ignas/langlearn/core/DataImporterExporter.java
+++ LangLearn/src/main/java/uk/ignas/langlearn/core/DataImporterExporter.java
@@ -2,12 +2,17 @@ package uk.ignas.langlearn.core;
 
 import android.content.Context;
 import android.os.Environment;
+import android.widget.Toast;
+import com.google.common.collect.Sets;
+import uk.ignas.langlearn.core.db.DBHelper;
 import uk.ignas.langlearn.core.parser.DbUtils;
+import uk.ignas.langlearn.core.parser.TranslationParser;
 
-import java.io.File;
+import java.io.*;
+import java.util.*;
 
 public class DataImporterExporter {
-
+    private TranslationParser translationParser = new TranslationParser();
     private Context context;
 
     public DataImporterExporter(Context context) {
@@ -26,9 +31,9 @@ public class DataImporterExporter {
         }
 
         try {
-            new DbUtils(context).importFromFile(dataToImportFile.getAbsolutePath());
+            importFromFile(dataToImportFile.getAbsolutePath());
             reexport(exportedDataFileName);
-            new DbUtils(context).validateImportAndExportWorksConsistently(dataToImportFile.getAbsolutePath(), exportedDataFile.getAbsolutePath());
+            validateImportAndExportWorksConsistently(dataToImportFile.getAbsolutePath(), exportedDataFile.getAbsolutePath());
 
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -47,6 +52,107 @@ public class DataImporterExporter {
                 throw new RuntimeException("data cannot be exported. File cannot be deleted");
             }
         }
-        new DbUtils(context).export(planeTextExportedFile.getAbsolutePath());
+        export(planeTextExportedFile.getAbsolutePath());
+    }
+
+    public void importFromFile(String planeTextFilePath) throws IOException {
+        List<String> planeText = readFile(planeTextFilePath);
+
+        DBHelper dbHelper = new DBHelper(context);
+        LinkedHashSet<Translation> translationsToInsert = new LinkedHashSet<>();
+
+        for (String planeTextLine : planeText) {
+            Translation parsed = translationParser.parse(planeTextLine);
+            if (parsed != null) {
+                translationsToInsert.add(parsed);
+            }
+        }
+
+        Set<Translation> translationsFromDb = new DbUtils(context).getTranslationsFromDb().keySet();
+        Sets.SetView<Translation> toAdd = Sets.difference(translationsToInsert, translationsFromDb);
+        Sets.SetView<Translation> toRemove = Sets.difference(translationsFromDb, translationsToInsert);
+
+        List<Translation> reversedToAdd = new ArrayList<>(toAdd);
+        Collections.reverse(reversedToAdd);
+
+        dbHelper.insert(reversedToAdd);
+        dbHelper.delete(toRemove);
+
+        showToast("inserted: " + toAdd.size() + "; removed: " + toRemove.size());
+    }
+
+    private void showToast(String text) {
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void export(String planeTextExportedPath) {
+        LinkedHashMap<Translation, Difficulty> translationsFromDb = new DbUtils(context).getTranslationsFromDb();
+        try {
+            writeTranslations(planeTextExportedPath, translationsFromDb.keySet());
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void validateImportAndExportWorksConsistently(String dataToImportFileName, String exportedDataFileName) {
+        if (!new File(dataToImportFileName).exists()) {
+            throw new RuntimeException("validation failed. invalid import file specified");
+        }
+        if (!new File(exportedDataFileName).exists()) {
+            throw new RuntimeException("validation failed. invalid export file specified");
+        }
+        List<String> dataToImport = readFile(dataToImportFileName);
+        List<String> exportedData = readFile(exportedDataFileName);
+
+        for(Iterator<String> it = dataToImport.iterator(); it.hasNext(); ) {
+            if (it.next().isEmpty()) {
+                it.remove();
+            }
+        }
+
+        Set<String> dataToImportSet = new HashSet<>(dataToImport);
+        Set<String> exportedDataSet = new HashSet<>(exportedData);
+        if (!dataToImportSet.equals(exportedDataSet)) {
+            throw new RuntimeException("Import or export does not work properly");
+        }
+    }
+
+
+
+    private List<String> readFile(String path) {
+        BufferedReader br = null;
+        List<String> lines = new ArrayList<>();
+        try {
+            String sCurrentLine;
+            br = new BufferedReader(new FileReader(path));
+            while ((sCurrentLine = br.readLine()) != null) {
+                lines.add(sCurrentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return lines;
+    }
+
+    private void writeTranslations(String path, Set<Translation> translations) throws IOException {
+        File fout = new File(path);
+        FileOutputStream fos = new FileOutputStream(fout);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+
+        for (Translation translation : translations) {
+            bw.write(translation.getTranslatedWord() + " - " + translation.getOriginalWord());
+            bw.newLine();
+        }
+
+        bw.close();
     }
 }
--- LangLearn/src/main/java/uk/ignas/langlearn/core/parser/DbUtils.java
+++ LangLearn/src/main/java/uk/ignas/langlearn/core/parser/DbUtils.java
@@ -1,17 +1,17 @@
 package uk.ignas.langlearn.core.parser;
 
 import android.content.Context;
-import android.widget.Toast;
-import com.google.common.collect.Sets;
 import uk.ignas.langlearn.core.Difficulty;
 import uk.ignas.langlearn.core.Translation;
 import uk.ignas.langlearn.core.db.DBHelper;
 
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public class DbUtils {
-    private TranslationParser translationParser = new TranslationParser();
+
     private final Context context;
 
     public DbUtils(Context context) {
@@ -29,102 +29,5 @@ public class DbUtils {
         return allTranslationsReversed;
     }
 
-    public void importFromFile(String planeTextFilePath) throws IOException {
-        List<String> planeText = readFile(planeTextFilePath);
 
-        DBHelper dbHelper = new DBHelper(context);
-        LinkedHashSet<Translation> translationsToInsert = new LinkedHashSet<>();
-
-        for (String planeTextLine : planeText) {
-            Translation parsed = translationParser.parse(planeTextLine);
-            if (parsed != null) {
-                translationsToInsert.add(parsed);
-            }
-        }
-
-        Set<Translation> translationsFromDb = getTranslationsFromDb().keySet();
-        Sets.SetView<Translation> toAdd = Sets.difference(translationsToInsert, translationsFromDb);
-        Sets.SetView<Translation> toRemove = Sets.difference(translationsFromDb, translationsToInsert);
-
-        List<Translation> reversedToAdd = new ArrayList<>(toAdd);
-        Collections.reverse(reversedToAdd);
-
-        dbHelper.insert(reversedToAdd);
-        dbHelper.delete(toRemove);
-
-        showToast("inserted: " + toAdd.size() + "; removed: " + toRemove.size());
-    }
-
-    public void export(String planeTextExportedPath) {
-        LinkedHashMap<Translation, Difficulty> translationsFromDb = getTranslationsFromDb();
-        try {
-            writeTranslations(planeTextExportedPath, translationsFromDb.keySet());
-        } catch (Exception e) {
-            throw new RuntimeException();
-        }
-    }
-
-    public void validateImportAndExportWorksConsistently(String dataToImportFileName, String exportedDataFileName) {
-        if (!new File(dataToImportFileName).exists()) {
-            throw new RuntimeException("validation failed. invalid import file specified");
-        }
-        if (!new File(exportedDataFileName).exists()) {
-            throw new RuntimeException("validation failed. invalid export file specified");
-        }
-        List<String> dataToImport = readFile(dataToImportFileName);
-        List<String> exportedData = readFile(exportedDataFileName);
-
-        for(Iterator<String> it = dataToImport.iterator(); it.hasNext(); ) {
-            if (it.next().isEmpty()) {
-                it.remove();
-            }
-        }
-
-        Set<String> dataToImportSet = new HashSet<>(dataToImport);
-        Set<String> exportedDataSet = new HashSet<>(exportedData);
-        if (!dataToImportSet.equals(exportedDataSet)) {
-            throw new RuntimeException("Import or export does not work properly");
-        }
-    }
-
-    private void showToast(String text) {
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-    }
-
-    private List<String> readFile(String path) {
-        BufferedReader br = null;
-        List<String> lines = new ArrayList<>();
-        try {
-            String sCurrentLine;
-            br = new BufferedReader(new FileReader(path));
-            while ((sCurrentLine = br.readLine()) != null) {
-                lines.add(sCurrentLine);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (br != null) br.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-        return lines;
-    }
-
-    private void writeTranslations(String path, Set<Translation> translations) throws IOException {
-        File fout = new File(path);
-        FileOutputStream fos = new FileOutputStream(fout);
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-
-        for (Translation translation : translations) {
-            bw.write(translation.getTranslatedWord() + " - " + translation.getOriginalWord());
-            bw.newLine();
-        }
-
-        bw.close();
-    }
 }
