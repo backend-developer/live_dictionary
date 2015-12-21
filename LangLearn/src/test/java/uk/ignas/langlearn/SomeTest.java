0f1e242de2e0669b0baa377d02b1846f19297f8f
TIgnas <java.backend.developer@outlook.com>
2015-12-21 15:35:13 +0000

    #36 [Ignas] optimize imports

--- LangLearn/src/test/java/uk/ignas/langlearn/SomeTest.java
+++ LangLearn/src/test/java/uk/ignas/langlearn/SomeTest.java
@@ -3,11 +3,6 @@ package uk.ignas.langlearn;
 import junit.framework.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by ignas on 12/16/15.
- */
 public class SomeTest {
     @Test
     public void someTest() {
