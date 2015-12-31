d239bfc1b3873c39999fe7b72b2b51193565a48f
TIgnas <java.backend.developer@outlook.com>
2015-12-31 20:24:16 +0000

    #54 [Ignas] naming

--- LangLearn/src/main/java/uk/ignas/langlearn/core/Reminder.java
+++ LangLearn/src/main/java/uk/ignas/langlearn/core/Reminder.java
@@ -5,11 +5,6 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Reminder {
-    private static final int TIMES_TO_SUCCEED_IN_0_LEVEL_FOR_STAGING = 2;
-    private static final int TIMES_TO_SUCCEED_IN_1_LEVEL_FOR_STAGING = 3;
-
-    private static final int PERIOD_IN_HOURS_TO_REACH_LEVEL_2 = 4;
-
     private final Clock clock;
 
     public Reminder(Clock clock) {
@@ -17,43 +12,45 @@ public class Reminder {
     }
 
     public boolean shouldBeReminded(TranslationMetadata metadata) {
-        int level1 = 0;
-        List<DifficultyAtTime> subset = new ArrayList<>();
-        for (DifficultyAtTime difficultyAtTime : metadata.getRecentDifficulty()) {
-            if (difficultyAtTime.getDifficulty() == Difficulty.DIFFICULT) {
-                level1 = 1;
-
-                subset.clear();
-            } else {
-
-                subset.add(difficultyAtTime);
-            }
-        }
-        int counterForLastCorrectSequence = subset.size();
+        List<DifficultyAtTime> successAfterLastFailure = getSuccessLogAfterLastFailure(metadata);
+        boolean wasEverFailed = successAfterLastFailure.size() != metadata.getRecentDifficulty().size();
+        int counterForLastCorrectSequence = successAfterLastFailure.size();
         boolean canRemind = false;
-        if (level1 == 1) {
+        if (wasEverFailed) {
             if (counterForLastCorrectSequence <= 3) {
-                if (counterForLastCorrectSequence <= 2 || countInNRecentHours(subset.subList(0, 3), 4) <= 2) {
+                if (counterForLastCorrectSequence <= 2 || countInNRecentHours(successAfterLastFailure.subList(0, 3), 4) <= 2) {
                     canRemind = true;
                 }
             } else if (counterForLastCorrectSequence <= 5) {
-                if (counterForLastCorrectSequence <= 4 || countInNRecentHours(subset.subList(3, 5), 20) <= 1) {
+                if (counterForLastCorrectSequence <= 4 || countInNRecentHours(successAfterLastFailure.subList(3, 5), 20) <= 1) {
                     canRemind = true;
                 }
             }
-        } else if (level1 == 0) {
+        } else {
             if (counterForLastCorrectSequence <= 2) {
-                if (counterForLastCorrectSequence <= 1 || countInNRecentHours(subset.subList(0, 2), 4) <= 1) {
+                if (counterForLastCorrectSequence <= 1 || countInNRecentHours(successAfterLastFailure.subList(0, 2), 4) <= 1) {
                     canRemind = true;
                 }
             } else if (counterForLastCorrectSequence <= 4) {
-                if (counterForLastCorrectSequence <= 3 || countInNRecentHours(subset.subList(2, 4), 20) <= 1) {
+                if (counterForLastCorrectSequence <= 3 || countInNRecentHours(successAfterLastFailure.subList(2, 4), 20) <= 1) {
                     canRemind = true;
                 }
             }
         }
 
         return canRemind;
+    }
+
+    private List<DifficultyAtTime> getSuccessLogAfterLastFailure(TranslationMetadata metadata) {
+        List<DifficultyAtTime> successLogAfterLastFailure = new ArrayList<>();
+        for (DifficultyAtTime difficultyAtTime : metadata.getRecentDifficulty()) {
+            if (difficultyAtTime.getDifficulty() == Difficulty.DIFFICULT) {
+                successLogAfterLastFailure.clear();
+            } else {
+                successLogAfterLastFailure.add(difficultyAtTime);
+            }
+        }
+        return successLogAfterLastFailure;
     }
 
     private int countInNRecentHours(List<DifficultyAtTime> difficultyAtTimes, int hours) {
