f2a59a75d03a4d88689099c0f953b58327493e9a
TIgnas <java.backend.developer@outlook.com>
2016-01-01 12:03:47 +0000

    #54 [Ignas] cleanup refactoring

--- LangLearn/src/main/java/uk/ignas/langlearn/core/Reminder.java
+++ LangLearn/src/main/java/uk/ignas/langlearn/core/Reminder.java
@@ -12,48 +12,80 @@ public class Reminder {
         this.clock = clock;
     }
 
+    private static class MsgCountAndNumOfHours {
+        private final int msgCount;
+        private final int numOfHours;
+
+        public MsgCountAndNumOfHours(int msgCount, int numOfHours) {
+            this.msgCount = msgCount;
+            this.numOfHours = numOfHours;
+        }
+    }
+
     public boolean shouldBeReminded(TranslationMetadata metadata) {
         List<DifficultyAtTime> successAfterLastFailure = getSuccessLogAfterLastFailure(metadata);
         boolean wasEverFailed = successAfterLastFailure.size() != metadata.getRecentDifficulty().size();
         int counterForLastCorrectSequence = successAfterLastFailure.size();
-        boolean canRemind = false;
+        boolean shouldRemind = false;
         if (wasEverFailed) {
             if (counterForLastCorrectSequence <= 3) {
                 if (counterForLastCorrectSequence <= 2 || countInNRecentHours(successAfterLastFailure.subList(0, 3), 4) <= 2) {
-                    canRemind = true;
+                    shouldRemind = true;
                 }
             } else if (counterForLastCorrectSequence <= 5) {
                 if (counterForLastCorrectSequence <= 4 || countInNRecentHours(successAfterLastFailure.subList(3, 5), 20) <= 1) {
-                    canRemind = true;
+                    shouldRemind = true;
                 }
             }
         } else {
+            List<List<DifficultyAtTime>> groups = findPairsFittingNHoursPeriodInOrder(successAfterLastFailure,
+                    new MsgCountAndNumOfHours(2, 4),
+                    new MsgCountAndNumOfHours(2, 20));
             int promotionLevel = 1;
-            List<Integer> foundMessageIndexesLevel1 = findIndexesForFirstTwoMessagesSubmittedWithinNHours(successAfterLastFailure, 4);
-            if (foundMessageIndexesLevel1.isEmpty()) {
-                canRemind = true;
-            } else {
-                if (TimeUnit.MILLISECONDS.toHours(clock.getTime().getTime() - successAfterLastFailure.get(0).getTimepoint().getTime()) < 4) {
-                    canRemind = false;
-                } else {
-                    canRemind = true;
-                    promotionLevel++;
-                }
+
+            shouldRemind = groups.get(0).isEmpty() || !isMessageYoungerThanNHours(groups.get(0).get(0), 4);
+            boolean isLevelPromoted = !groups.get(0).isEmpty() && !isMessageYoungerThanNHours(groups.get(0).get(0), 4);
+            if (isLevelPromoted) {
+                promotionLevel++;
             }
             if (promotionLevel == 2) {
-                List<DifficultyAtTime> sublist = successAfterLastFailure.subList(foundMessageIndexesLevel1.get(1) + 1, successAfterLastFailure.size());
-                List<Integer> foundMessageIndexesLevel2 = findIndexesForFirstTwoMessagesSubmittedWithinNHours(sublist, 20);
-                if (foundMessageIndexesLevel2.isEmpty()) {
-                    canRemind = true;
-                } else if (TimeUnit.MILLISECONDS.toHours(clock.getTime().getTime() - sublist.get(foundMessageIndexesLevel2.get(0)).getTimepoint().getTime()) < 20) {
-                    canRemind = false;
+                if (groups.get(1).isEmpty()) {
+                    shouldRemind = true;
+                } else if (isMessageYoungerThanNHours(groups.get(1).get(0), 20)) {
+                    shouldRemind = false;
                 } else {
-                    canRemind = true;
+                    shouldRemind = true;
+                    promotionLevel++;
                 }
             }
         }
 
-        return canRemind;
+        return shouldRemind;
+    }
+
+    private boolean isMessageYoungerThanNHours(DifficultyAtTime difficultyAtTime, int hours) {
+        return TimeUnit.MILLISECONDS.toHours(clock.getTime().getTime() - difficultyAtTime.getTimepoint().getTime()) < hours;
+    }
+
+    private List<List<DifficultyAtTime>> findPairsFittingNHoursPeriodInOrder(List<DifficultyAtTime> difficulty, MsgCountAndNumOfHours msgCountAndNumOfHours, MsgCountAndNumOfHours msgCountAndNumOfHours1) {
+        List<List<DifficultyAtTime>> groups = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            groups.add(new ArrayList<DifficultyAtTime>());
+        }
+
+        List<Integer> indiceForGroupZero = findIndexesForFirstPairSubmittedWithinNHours(difficulty, 4);
+        for (Integer index : indiceForGroupZero) {
+            groups.get(0).add(difficulty.get(index));
+        }
+        if (!groups.get(0).isEmpty()) {
+            List<DifficultyAtTime> sublist = difficulty.subList(indiceForGroupZero.get(1) + 1, difficulty.size());
+            List<Integer> indiceForGroupOne = findIndexesForFirstPairSubmittedWithinNHours(sublist, 20);
+
+            for (Integer index : indiceForGroupOne) {
+                groups.get(1).add(sublist.get(index));
+            }
+        }
+        return groups;
     }
 
     private List<DifficultyAtTime> getSuccessLogAfterLastFailure(TranslationMetadata metadata) {
@@ -68,13 +100,13 @@ public class Reminder {
         return successLogAfterLastFailure;
     }
 
-    private List<Integer> findIndexesForFirstTwoMessagesSubmittedWithinNHours(List<DifficultyAtTime> difficultyAtTimes, int hours) {
+    private List<Integer> findIndexesForFirstPairSubmittedWithinNHours(List<DifficultyAtTime> difficultyAtTimes, int hours) {
         List<Integer> foundMessages = new ArrayList<>();
         for (int i = 1; i < difficultyAtTimes.size(); i++) {
             Date iDate = difficultyAtTimes.get(i).getTimepoint();
-            Date iMinus1Date = difficultyAtTimes.get(i-1).getTimepoint();
+            Date iMinus1Date = difficultyAtTimes.get(i - 1).getTimepoint();
             if (TimeUnit.MILLISECONDS.toHours(iDate.getTime() - iMinus1Date.getTime()) < hours) {
-                foundMessages.add(i-1);
+                foundMessages.add(i - 1);
                 foundMessages.add(i);
             } else {
                 foundMessages.clear();
@@ -90,7 +122,7 @@ public class Reminder {
         int counter = 0;
         for (DifficultyAtTime difficultyAtTime : difficultyAtTimes) {
             if (difficultyAtTime.getDifficulty() == Difficulty.EASY) {
-                if (TimeUnit.MILLISECONDS.toHours(clock.getTime().getTime() - difficultyAtTime.getTimepoint().getTime()) < hours) {
+                if (isMessageYoungerThanNHours(difficultyAtTime, hours)) {
                     counter++;
                 }
             } else {
