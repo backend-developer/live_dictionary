86055176a383daca6c768b12fb86bdf1fdca0134
TIgnas <java.backend.developer@outlook.com>
2016-01-01 10:52:57 +0000

    #54 [Ignas] Removed comments

--- LangLearn/src/main/java/uk/ignas/langlearn/core/Reminder.java
+++ LangLearn/src/main/java/uk/ignas/langlearn/core/Reminder.java
@@ -33,29 +33,19 @@ public class Reminder {
                 canRemind = true;
             }
             if (!foundMessageIndexesLevel1.isEmpty()) {
-//                Integer index = foundMessageIndexesLevel1.get(1);
-//                int nextIndex = index + 1; // 2
-//                int size = nextIndex + 2;  // 4
-//                int sizeMinus1 = size - 1 ; // 3
                 if (TimeUnit.MILLISECONDS.toHours(clock.getTime().getTime() - successAfterLastFailure.get(0).getTimepoint().getTime()) < 4) {
                     canRemind = false;
                 } else {
-                    canRemind = true;
                     List<DifficultyAtTime> sublist = successAfterLastFailure.subList(foundMessageIndexesLevel1.get(1) + 1, successAfterLastFailure.size());
                     List<Integer> foundMessageIndexesLevel2 = findIndexesForFirstTwoMessagesSubmittedWithinNHours(sublist, 20);
                     if (foundMessageIndexesLevel2.isEmpty()) {
                         canRemind = true;
                     } else if (TimeUnit.MILLISECONDS.toHours(clock.getTime().getTime() - sublist.get(foundMessageIndexesLevel2.get(0)).getTimepoint().getTime()) < 20) {
                         canRemind = false;
+                    } else {
+                        canRemind = true;
                     }
                 }
-//                else if (counterForLastCorrectSequence <= size) {
-//                    if (counterForLastCorrectSequence <= sizeMinus1 || countInNRecentHours(successAfterLastFailure.subList(nextIndex, size), 20) <= 1) {
-//                        canRemind = true;
-//                    } else {
-//                        canRemind = false;
-//                    }
-//                }
             }
         }
 
